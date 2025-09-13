9cb5a60b1b1a63f7c6659c06ae6a9134e0ee73db
mapeflo <mpenarandaf@unicartagena.edu.co>
2025-09-13 00:25:48 -0500

    Avance del taller 5 con inicio del ejercicio 1.

---
+++ src/taller5/Producto.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package taller5;
+
+/**
+ *
+ * @author marya
+ */
+  class Producto {
+      
+        String nombre;
+        double precio;
+        int stock;
+
+  Producto(String nombre, double precio, int stock) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.stock = stock;
+    }
+       void mostrarInfo()  {
+     
+           System.out.println("Nombre: " + nombre + ",Precio: " + precio + ",Stock: " + stock);
+    }  
+        
+}
